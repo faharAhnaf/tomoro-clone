1782545dc23fec82b958b3621c9223433409c90a
Fahar Ahnaf Azis <fhrahnv@Fahars-MacBook-Air.local>
2024-07-25 21:27:41 +0700

    Adding a atomic design

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <p>jangan makan nasi</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
