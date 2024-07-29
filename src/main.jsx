107ce5c12f5d6b883e1d1e56d19a30ede1595963
Fahar Ahnaf Azis <faharahnaf66@gmail.com>
2024-07-29 23:30:03 +0700

    Adding a Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import "./index.css";
+import LandingPage from "./components/pages/LandingPage.jsx";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage></LandingPage>,
+    },
+    {
+      path: "/about",
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
